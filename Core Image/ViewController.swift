6c08b0c10936134cea993cffccaf5dcdfcda9534
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-12-01 11:36:13 +0800

    step 2: do preparation for image view step 3: write codes for image processing in ViewController

--- Core Image/ViewController.swift
+++ Core Image/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        guard let image = imageView?.image, let cgimg = image.cgImage else {
+            print("image view doesn't have an image")
+            return
+        }
+        
+        let coreImage = CIImage(cgImage: cgimg)
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(coreImage, forKey: kCIInputImageKey)
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        
+        if let output = filter?.value(forKey: kCIOutputImageKey) as?  CIImage{
+            let filteredImage = UIImage(ciImage: output)
+            imageView?.image = filteredImage
+        } else {
+            print("Image filtering failed")
+        }
     }
 
     override func didReceiveMemoryWarning() {
