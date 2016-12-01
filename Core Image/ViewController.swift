e10045572581a7ec9b82fd1f86418b0dc8e5f81b
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-12-01 15:55:53 +0800

    Step 4 使用 GPU 改善效能

--- Core Image/ViewController.swift
+++ Core Image/ViewController.swift
@@ -25,6 +25,14 @@ class ViewController: UIViewController {
             return
         }
         
+        /* 為效能問題, 將影像處理透過 CIContext 由CPU 轉到 GPU 處理 -- Step A  */
+        // 建立新的 OpenGL ES context, 它是一個高速的圖形API, 由 GPU 直接產生
+        // 此例使用的是 OpenGL ES V2 的版本. 大部份 iOS 設備支援此版本. 
+        // 最新出到 ES V3. 如果設備有支援, 改為 openGLES3 即可
+        let openGLContext = EAGLContext(api: .openGLES2)
+        // 因為我們不能直接使用 OpenGL ES context 做影像過濾, 所以在這將它轉為 CIContext
+        let context = CIContext(eaglContext: openGLContext!)
+        
         //Core Image 是作用在 CIImage, 而不是 UIImage. 所以，所有的UIImage 必須都轉成 CIImage
         let coreImage = CIImage(cgImage: cgimg)
         
@@ -38,10 +46,22 @@ class ViewController: UIViewController {
         
         //呼叫 CoreImage 執行照片過濾, 並強制轉為 CIImage 格式
         if let output = filter?.value(forKey: kCIOutputImageKey) as?  CIImage{
+            
+        /* CPU 的處理程序
             //將 CIImage 轉為 UIImage, 準備做輸出使用
             let filteredImage = UIImage(ciImage: output)
             //將 ImageView 的圖片, 改為過濾過的圖片
             imageView?.image = filteredImage
+        */
+        
+        /*  呼叫 GPU 執行影像處理 Step B  */
+            // V1: let cgimgresult = context.createCGImage(output, fromRect: output.extent)
+            // V2:  
+            // 在此我們使用的是 CGImage 的影像, 而不是 CIImage. V1 的 fromRect 是指定影像大小, 但V2 不支援
+            // extent 代表的是 "影像大小(image size)"
+            let cgimgresult = context.createCGImage(output, from: output.extent)
+            let result = UIImage(cgImage: cgimgresult!)
+            imageView?.image = result
         } else {
             print("Image filtering failed")
         }
